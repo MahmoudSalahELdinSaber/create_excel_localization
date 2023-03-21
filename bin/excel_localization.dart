291094ca1e648dda2c7ec7ae50031085a1697da7
MahmoudSalahELdinSaber <msalaheldin@kn-it.com>
2023-03-21 16:44:53 +0200

    refactor code

--- bin/excel_localization.dart
+++ bin/excel_localization.dart
@@ -4,39 +4,69 @@ import 'package:quartet/quartet.dart';
 
 void main(List<String> arguments) async {
   const filePathInput = 'excel/input.xlsx';
+  final excel = getExcel(filePathInput);
+  final sheet = getFirstSheet(excel);
+  final firstRow = getFirstRow(sheet);
+  final fileBytes = createKeysRow(firstRow, excel);
   const filePathOutput = 'excel/output.xlsx';
-  const fileKeysPath = 'excel/keys.text';
-  final bytes = File(filePathInput).readAsBytesSync();
-  final excel = Excel.decodeBytes(bytes);
+  saveExcelfile(filePathOutput, fileBytes);
+  final keys = createKeysText(excel);
+  saveKeysFile(keys);
+}
 
-  for (var table in excel.tables.entries) {
-    final sheet = table.value;
-    final rows = sheet.rows;
-    final newValues = <String>[];
-    var firstRow = rows.first;
-    for (var cell in firstRow) {
-      var key = cell.value.toString().trim();
-      key = camelCase(key);
-      final regex =
-          r'[^\p{Alphabetic}\p{Mark}\p{Decimal_Number}\p{Connector_Punctuation}\p{Join_Control}\s]+';
-      key = key.replaceAll(RegExp(regex, unicode: true), '');
-      newValues.add(key);
-    }
-    print(newValues);
-    excel.sheets.entries.first.value.insertRow(0);
-    excel.sheets.entries.first.value.insertRowIterables(newValues, 0);
-  }
-  var fileBytes = excel.save();
-  File(filePathOutput)
+void saveKeysFile(String keys) {
+  const fileKeysPath = 'excel/keys.text';
+  File(fileKeysPath)
     ..createSync(recursive: true)
-    ..writeAsBytesSync(fileBytes);
+    ..writeAsString(keys);
+}
+
+String createKeysText(Excel excel) {
   var keys = '';
   for (final e in excel.sheets.entries.first.value.rows.first
       .map((e) => e.value)
       .toSet()) {
     keys += 'const $e = "$e";' '\n';
   }
-  File(fileKeysPath)
+  return keys;
+}
+
+void saveExcelfile(String filePathOutput, List<int> fileBytes) {
+  File(filePathOutput)
     ..createSync(recursive: true)
-    ..writeAsString(keys);
+    ..writeAsBytesSync(fileBytes);
+}
+
+List<int> createKeysRow(List<Data> firstRow, Excel excel) {
+  final newValues = <String>[];
+  for (var cell in firstRow) {
+    var key = cell.value.toString().trim();
+    key = camelCase(key);
+    final regex =
+        r'[^\p{Alphabetic}\p{Mark}\p{Decimal_Number}\p{Connector_Punctuation}\p{Join_Control}\s]+';
+    key = key.replaceAll(RegExp(regex, unicode: true), '');
+    newValues.add(key);
+  }
+  excel.sheets.entries.first.value.insertRow(0);
+  excel.sheets.entries.first.value.insertRowIterables(newValues, 0);
+  var fileBytes = excel.save();
+  return fileBytes;
+}
+
+List<Data> getFirstRow(Sheet sheet) {
+  final rows = sheet.rows;
+  final firstRow = rows.first;
+  return firstRow;
+}
+
+Sheet getFirstSheet(Excel excel) {
+  final table = excel.tables.entries.first;
+  final sheet = table.value;
+  return sheet;
+}
+
+Excel getExcel(String filePathInput) {
+  final bytes = File(filePathInput).readAsBytesSync();
+  final excel = Excel.decodeBytes(bytes);
+  return excel;
 }
